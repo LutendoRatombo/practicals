9e2d465edaaa735c171beada0fa216e3fe49ed2b
Lutendo Ratombo <25033493@mvula.univen.ac.za>
2026-05-04 07:59:37 -0700

    first commit

---
+++ src/main/java/com/mycompany/loginform3/LoginForm3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginform3;
+
+/**
+ *
+ * @author 25033493
+ */
+public class LoginForm3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
